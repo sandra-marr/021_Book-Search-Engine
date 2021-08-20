5363cf462c787fa767a0e0654d2fb6142ca1455b
SPeats <srpeters44@gmail.com>
2021-08-20 14:08:56 -0700

    updated server.js file to include the apollo server and apply it as middleware

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 021_Book-Search-Engine
+
+steps for install 
+clone the repo, npm i in the client and then server folders, 
--- server/server.js
+++ server/server.js
@@ -3,10 +3,21 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -17,5 +28,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+    
 });
+
+// console.log(`🌍 Now listening on localhost:${PORT}`));
